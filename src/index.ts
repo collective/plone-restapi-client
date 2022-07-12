6219c0a340faf83d054d795819f235a03807c87d
nzambello <nzambello@protonmail.com>
2022-07-12 17:33:59 +0200

    feat: refactor + add methods for content, add types for plone content

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-// export * as client from './client';
-// export * as content from './content';
-// export * as comments from './comments';
-
 import * as client from './client';
 import * as content from './content';
 import * as comments from './comments';
--- test/content.test.ts
+++ test/content.test.ts
@@ -1,12 +1,93 @@
-import { init } from '../src/client';
-import { get } from '../src/content';
+import { init, auth, logout } from '../src/client';
+import * as content from '../src/content';
 
 init('http://localhost:8080/Plone');
 
+afterEach(async () => {
+  await logout();
+});
+
 it('gets content for the homepage', async () => {
-  const res = await get('/');
+  const res = await content.get('/');
+  expect(res.status).toBe(200);
   expect(res['@type']).toEqual('Plone Site');
-  const news = await get('/news');
-  expect(news['@type']).toEqual('Folder');
-  expect(news.title).toEqual('Notizie');
+  expect(res['@id']).toEqual('http://localhost:8080/Plone');
+  expect(res.title).toEqual('Welcome to Volto!');
+});
+
+it('gets content for the homepage as admin', async () => {
+  const { token } = await auth('admin', 'admin');
+  const res = await content.get('/');
+  expect(res.status).toBe(200);
+  expect(res['@type']).toEqual('Plone Site');
+  expect(res['@id']).toEqual('http://localhost:8080/Plone');
+
+  expect(token).toBeDefined();
+  await logout();
+});
+
+it('gets non existing content', async () => {
+  const res = await content.get('awanagana');
+  expect(res.status).toBe(404);
+  expect(res.type).toEqual('NotFound');
+});
+
+it('creates a page', async () => {
+  const { token } = await auth('admin', 'admin');
+  expect(token).toBeDefined();
+
+  const res = await content.add('/', {
+    '@type': 'Document',
+    title: 'Test Page',
+  });
+  expect(res['@type']).toEqual('Document');
+  expect(res['@id']).toEqual('http://localhost:8080/Plone/test-page');
+  expect(res.title).toEqual('Test Page');
+  expect(res.status).toBe(201);
+});
+
+it('updates a page', async () => {
+  const { token } = await auth('admin', 'admin');
+  expect(token).toBeDefined();
+
+  const res = await content.update(
+    '/test-page',
+    {
+      title: 'Test Page',
+      description: 'Test Page Description',
+    },
+    {
+      headers: {
+        Prefer: 'return=representation',
+      },
+    }
+  );
+  expect(res['@type']).toEqual('Document');
+  expect(res['@id']).toEqual('http://localhost:8080/Plone/test-page');
+  expect(res.title).toEqual('Test Page');
+  expect(res.status).toBe(200);
+});
+
+it('gets private content', async () => {
+  const res = await content.get('/test-page');
+  expect(res.status).toBe(401);
+  expect(res.type).toEqual('Unauthorized');
+});
+
+it('gets private content once logged in', async () => {
+  const { token } = await auth('admin', 'admin');
+  expect(token).toBeDefined();
+
+  const res = await content.get('/test-page');
+  expect(res.status).toBe(200);
+  expect(res['@id']).toEqual('http://localhost:8080/Plone/test-page');
+  expect(res.title).toEqual('Test Page');
+});
+
+it('deletes content', async () => {
+  const { token } = await auth('admin', 'admin');
+  expect(token).toBeDefined();
+
+  const res = await content.remove('/test-page');
+  expect(res.status).toBe(204);
 });
